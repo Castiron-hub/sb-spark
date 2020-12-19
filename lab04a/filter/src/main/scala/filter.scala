39cb256193edd00b92be62bfe6978068b36eb67d
Castiron-hub <ivshish@mail.ru>
2020-12-19 16:52:37 +0300

    lab04a

--- lab04a/filter/src/main/scala/filter.scala
+++ lab04a/filter/src/main/scala/filter.scala
@@ -1,73 +1,75 @@
-import org.apache.spark.sql.types.{DataTypes, StructType}
-import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions.{lit, from_json, col, from_unixtime}
-import org.apache.spark.sql.{Dataset, DataFrame}
+import org.apache.hadoop.conf.Configuration
+import org.apache.spark.sql.streaming.Trigger
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.functions.{expr,col, date_format, from_json, from_unixtime, lit, struct, to_date}
+import org.apache.hadoop.fs.{FileSystem, Path}
+import org.apache.spark.sql.types.{IntegerType, LongType, StringType, StructField, StructType, TimestampType}
+import scala.reflect.io.Directory
 import java.io.File
+import java.net._
 
-object filter extends App {
 
-  val kafkaAddress: String = "spark-master-1:6667"
-  def writeKafka[T](topic: String, data: Dataset[T]): Unit = {
+object filter {
+
+  def deleteRecursively(file: File): Unit = {
+    if (file.isDirectory) file.listFiles.foreach(deleteRecursively)
+    if (file.exists && !file.delete) throw new Exception(s"Unable to delete ${file.getAbsolutePath}")
+  }
+
+  def clean_target(path:String, conf:Configuration) = {
+    val fs = FileSystem.get(conf)
+    val outPutPath = new Path(path)
+    if (fs.exists(outPutPath))
+      fs.delete(outPutPath, true)
+  }
+
+  def upload_topic(spark:SparkSession, topic_name:String, offset: String, output_dir_prefix:String, filter: String):Unit = {
+    var kafka_offset = "earliest"
+    if (offset != "earliest") kafka_offset =  s""" { "$topic_name": { "0": $offset } } """
     val kafkaParams = Map(
-      "kafka.bootstrap.servers" -> kafkaAddress
+      "kafka.bootstrap.servers" -> "spark-master-1:6667",
+      "subscribe" -> topic_name,
+      "startingOffsets" ->  kafka_offset
     )
 
-    data.toJSON.withColumn("topic", lit(topic)).write.format("kafka").options(kafkaParams).save
+    val schema = StructType(Seq(
+      StructField("event_type", StringType, true),
+      StructField("category", StringType, true),
+      StructField("item_id", StringType, true),
+      StructField("item_price", IntegerType, true),
+      StructField("uid", StringType, true),
+      StructField("value", StringType, true),
+      StructField("timestamp", LongType, true)
+    ))
+
+    val sdf = spark.read.format("kafka").options(kafkaParams).load
+    val parsedSdf = sdf.select(col("value").cast("string").as("value")).withColumn("jsonData", from_json(col("value"), schema))
+      .drop("value")
+      .select(col("jsonData.*"))
+      .withColumn("date",date_format((col("timestamp")/1000).cast("timestamp") - expr("INTERVAL 3 HOURS"),"yyyyMMdd"))
+
+    parsedSdf.filter(col("event_type") === filter).write.format("json").partitionBy("date")
+      .option("path", output_dir_prefix+"/"+filter).save
   }
 
-  def readKafka(kafkaAddress: String, topic: String, offset:String ): DataFrame = {
-    val fullOffset = if (offset matches """\d+""") s""" { "$topic": { "0": $offset } } """ else offset
-  val kafkaParams = Map(
-        "kafka.bootstrap.servers" -> "spark-master-1:6667",
-        "subscribe" -> "lab04_input_data",
-        "startingOffsets" -> """earliest""",
-        "endingOffsets" -> """latest"""
-    )
+  def main(args: Array[String]): Unit = {
+    val spark = new SparkSession.Builder()
+      .appName("ap_lab04")
+      .enableHiveSupport()
+      .getOrCreate()
+    val topic = spark.conf.get("spark.filter.topic_name","lab04_input_data")
+    val offset = spark.conf.get("spark.filter.offset","earliest")
+    val prefix = spark.conf.get("spark.filter.output_dir_prefix")
 
-    val df = spark.read.format("kafka").options(kafkaParams).load
-    df
+    if(! (prefix contains "file:")) clean_target(prefix,spark.sparkContext.hadoopConfiguration)
+    else {
+      val uri = new URI(prefix)
+      val f = new File(uri)
+      deleteRecursively(f)
+    }
+    upload_topic(spark, topic, offset, prefix, "buy")
+    upload_topic(spark, topic, offset, prefix, "view")
+    println("check")
+    spark.stop()
   }
-
-  def deleteR(file: File) {
-    if (file.isDirectory)
-      Option(file.listFiles).map(_.toList).getOrElse(Nil).foreach(deleteR(_))
-    file.delete
-  }
-
-  val spark = SparkSession.builder
-    .appName("lab04-a-sts")
-    .getOrCreate()
-
-
-  val struct = new StructType()
-    .add("event_type", DataTypes.StringType)
-    .add("category", DataTypes.StringType)
-    .add("item_id", DataTypes.StringType)
-    .add("item_price", DataTypes.FloatType)
-    .add("uid", DataTypes.StringType)
-    .add("timestamp", DataTypes.TimestampType)
-
-
-  val topic_name = spark.conf.get("spark.filter.topic_name", "lab04_input_data")
-  val offset = spark.conf.get("spark.filter.offset", "earliest")
-  val output_dir_prefix = spark.conf.get("spark.filter.output_dir_prefix", "/user/ivan.shishkin/visits")
-  val df = readKafka(kafkaAddress, topic_name, offset)
-
-  val eventDf = df.select(from_json(col("value").cast("string"), struct).as("event"))
-
-  val eventDfplain = eventDf.select(col("event.*"))
-  val eventDfts = eventDfplain.select(col("*"), (col("timestamp").cast("long")/lit(1000) - lit(3) * lit(60) * lit(60)).alias("ts"))
-  val eventDfDate = eventDfts.select(col("*"), from_unixtime(col("ts"), "yyyyMMdd").alias("date"))
-
-  val eventList = List("buy", "view")
-  eventList.foreach((i: String) => {
-    deleteR(new File(s"/user/ivan.shishkin/visits/$i"))
-    eventDfDate
-      .filter(s"event_type = '$i'")
-      .write
-      .mode("overwrite")
-      .partitionBy("date")
-      .json(s"$output_dir_prefix/$i/")
-  })
-
 }
